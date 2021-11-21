f1be50c7b4748c05ceb32a7312715e93fdee8c81
EvgeniyHalimon <halimonevgeniy@gmail.com>
2021-11-21 23:48:44 +0200

    added mongoDB to project and mongoose, added models, change server logic

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Post = require('./models/post')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 
 const PORT = 3001
+const db = 'mongodb+srv://Fabulous:faceit13@cluster0.tuxps.mongodb.net/node-express?retryWrites=true&w=majority'
+
+mongoose
+.connect(db, {useNewUrlParser : true, useUnifiedTopology: true})
+.then((res) => console.log('Connetcted to DB'))
+.catch((error) => console.log(error))
 
 const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`)
 
@@ -63,14 +71,14 @@ app.get('/posts', (req, res) => {
 
 app.post('/add-post', (req, res) => {
   const { title, author, text } = req.body
-  const post = {
-    id: new Date(),
-    date: (new Date()).toLocaleDateString(),
-    title,
-    author,
-    text,
-  }
-  res.render(createPath('post'), { post, title })
+  const post = new Post({ title, author, text })
+  post
+  .save()
+  .then((result) => res.send(result))
+  .catch((error) => {
+      console.log(error)
+      res.render(createPath('error'), {title : 'Error'})
+  })
 })
 
 app.get('/add-post', (req, res) => {
